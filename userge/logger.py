60a67b4e28307dd9f23c5e52ef49807e3da8f09a
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-12-24 02:10:29 +0530

    Update logger.py

--- userge/logger.py
+++ userge/logger.py
@@ -1,27 +1,77 @@
-# pylint: disable=missing-module-docstring
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
 #
-# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
-__all__ = ['logging']
+import contextlib
+import glob
+import os
+from importlib import import_module
+from logging import Logger
 
-import logging
-from logging.handlers import RotatingFileHandler
+from . import LOGS
+from .fns.tools import get_all_files
 
-logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s - %(levelname)s] - %(name)s - %(message)s',
-                    datefmt='%d-%b-%y %H:%M:%S',
-                    handlers=[
-                        RotatingFileHandler(
-                            "logs/userge.log", maxBytes=20480, backupCount=10),
-                        logging.StreamHandler()
-                    ])
 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.parser.html").setLevel(logging.ERROR)
-logging.getLogger("pyrogram.session.session").setLevel(logging.ERROR)
+class Loader:
+    def __init__(self, path="plugins", key="Official", logger: Logger = LOGS):
+        self.path = path
+        self.key = key
+        self._logger = logger
+
+    def load(
+        self,
+        log=True,
+        func=import_module,
+        include=None,
+        exclude=None,
+        after_load=None,
+        load_all=False,
+    ):
+        _single = os.path.isfile(self.path)
+        if include:
+            if log:
+                self._logger.info("Including: {}".format("• ".join(include)))
+            files = glob.glob(f"{self.path}/_*.py")
+            for file in include:
+                path = f"{self.path}/{file}.py"
+                if os.path.exists(path):
+                    files.append(path)
+        elif _single:
+            files = [self.path]
+        else:
+            if load_all:
+                files = get_all_files(self.path, ".py")
+            else:
+                files = glob.glob(f"{self.path}/*.py")
+            if exclude:
+                for path in exclude:
+                    if not path.startswith("_"):
+                        with contextlib.suppress(ValueError):
+                            files.remove(f"{self.path}/{path}.py")
+        if log and not _single:
+            self._logger.info(
+                f"• Installing {self.key} Plugins || Count : {len(files)} •"
+            )
+        for plugin in sorted(files):
+            if func == import_module:
+                plugin = plugin.replace(".py", "").replace("/", ".").replace("\\", ".")
+            try:
+                modl = func(plugin)
+            except ModuleNotFoundError as er:
+                modl = None
+                self._logger.error(f"{plugin}: '{er.name}' not installed!")
+                continue
+            except Exception as exc:
+                modl = None
+                self._logger.error(f"pyUltroid - {self.key} - ERROR - {plugin}")
+                self._logger.exception(exc)
+                continue
+            if _single and log:
+                self._logger.info(f"Successfully Loaded {plugin}!")
+            if callable(after_load):
+                if func == import_module:
+                    plugin = plugin.split(".")[-1]
+                after_load(self, modl, plugin_name=plugin)
